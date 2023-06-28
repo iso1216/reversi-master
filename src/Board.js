ffb60313ef2cf97ed995264b07eb732bd6734639
Taishi <taishi2002i@gmail.com>
2023-06-28 10:16:52 +0900

    ボードの初期設定

---
+++ src/Board.js
@@ -0,0 +1,27 @@
+import Box from "./Box";
+import React from "react";
+
+class Board extends React.Component {
+  renderBox(i) {
+    return (
+      <Box
+        value={this.props.board[i]}
+      />
+    );
+  }
+
+  render() {
+    const edge = [0, 1, 2, 3, 4, 5, 6, 7];
+    return (
+      <div>
+        {edge.map((index) => (
+          <div className="board-row" key={index}>
+            {edge.map((number) => this.renderBox(number + 8 * index))}
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Board;
---
+++ src/Box.js
@@ -0,0 +1,9 @@
+function Box(props){
+  return (
+    <button className="box" >
+      {props.value}
+    </button>
+  );
+}
+
+export default Box;
